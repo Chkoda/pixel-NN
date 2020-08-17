6f62ef9e56b9073cfa98252aa75b67959c2f75b7
SaranshPK <saranshis@hotmail.com>
2020-08-17 13:12:16 -0700

    Updated Learning Rate

--- share/reference_number.py
+++ share/reference_number.py
@@ -10,7 +10,7 @@ def build_model(data_x, data_y):
         structure=[25, 20],
         hidden_activation=keras_utils.Sigmoid2,
         output_activation=keras_utils.Sigmoid2,
-        learning_rate=0.008,
+        learning_rate=0.08,
         weight_decay=1e-7,
         momentum=0.4,
         #minibatch_size=60,
