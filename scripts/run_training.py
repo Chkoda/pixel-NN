55144150eb68f97750234380f3b87ed5908b5587
SaranshPK <saranshis@hotmail.com>
2020-08-20 17:08:48 -0700

    changed print to logging

--- scripts/run_training.py
+++ scripts/run_training.py
@@ -69,13 +69,12 @@ def _main():
     logging.info(gpus)
 
     if gpus:
-        logging.info("In if statement")
         try:
             # Currently, memory growth needs to be the same across GPUs
             for gpu in gpus:
                 tf.config.experimental.set_memory_growth(gpu, True)
             logical_gpus = tf.config.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            logging.info(f'{len(gpus)} Physical GPUs, {len(logical_gpus)} Logical GPUs')
         except RuntimeError as e:
             # Memory growth must be set before GPUs have been initialized
             print(e)
