6760c9b611b278f29ab802e5f5e6c6b0a5c6a6d9
Chkoda <chkoda@uw.edu>
2020-08-28 18:25:05 -0700

    Fix apply output_number_estimated for confusion matrix, implement sigmoid activation

--- share/reference_number.py
+++ share/reference_number.py
@@ -2,14 +2,15 @@
 import sys
 sys.path.append('python')
 import keras_utils
+import keras
 
 def build_model(data_x, data_y):
     return keras_utils.simple_model(
         data_x,
         data_y,
         structure=[25, 20],
-        hidden_activation=keras_utils.Sigmoid2,
-        output_activation=keras_utils.Sigmoid2,
+        hidden_activation=keras.layers.Activation(keras.activations.sigmoid), #keras_utils.Sigmoid2,
+        output_activation=keras.layers.Activation(keras.activations.sigmoid), #keras_utils.Sigmoid2,
         learning_rate=0.08,
         weight_decay=1e-7,
         momentum=0.4,
